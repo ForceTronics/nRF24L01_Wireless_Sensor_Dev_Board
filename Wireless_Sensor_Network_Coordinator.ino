4d3df95a2f7be46fc4753f2ba81f3760e7534a2a
ForceTronics <forcetronics@gmail.com>
2016-01-06 16:53:32 -0700

    Create Wireless_Sensor_Network_Coordinator.ino
    This code is just meant to serve as a simple coordinator in an RF24Network to demonstrate the router and end device code. For more information on this project go to the ForceTronics YouTube Channel https://www.youtube.com/channel/UCNd_fNspAczm8UoE2ay7K1Q

---
+++ Wireless_Sensor_Network_Coordinator.ino
@@ -0,0 +1,54 @@
+
+#include <RF24Network.h>
+#include <RF24.h>
+#include <SPI.h>
+
+// start RF24 communication layer
+RF24 radio(9,10);
+
+// start RF24 network layer
+RF24Network network(radio);
+
+// Coordinator address
+const uint16_t thisNode = 00;
+
+// Structure of our payload coming from router and end devices
+struct Payload
+{
+  float aDCTemp; //temperature from onboard sensor
+  int aDCopen;  //open ADC port that returns measurement
+  bool batState; //bool to communicate battery power level, true is good and false means battery needs to be replaced
+};
+
+void setup(void)
+{
+  Serial.begin(57600);
+  Serial.println("Coordinator is online.....");
+ 
+  SPI.begin();
+  radio.begin();
+  network.begin(90, thisNode);
+}
+
+void loop(void)
+{
+  //check network communication regularly
+  network.update();
+
+   RF24NetworkHeader header; //create header variable
+   Payload payload; //create payload variable
+  // Any data on the network ready to read
+  while ( network.available() )
+  {
+    // If so, grab it and print it out
+    network.read(header,&payload,sizeof(payload));
+    Serial.print("The node this is from: ");
+    Serial.println(header.from_node);
+    Serial.print("Temperature: ");
+    Serial.print(payload.aDCTemp);
+    Serial.print(" Open ADC value: ");
+    Serial.print(payload.aDCopen);
+    Serial.print(" Battery status: ");
+    Serial.println(payload.batState);
+  }
+}
